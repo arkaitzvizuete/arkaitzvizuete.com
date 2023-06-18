b352014eaec0e0f37cae58347a2b386cb9a25f41
Arkaitz Vizuete <code@arkaitzvizuete.com>
2023-06-19 01:41:55 +0200

    Update TwentyThreePage.tsx

--- src/app/pages/TwentyThreePage.tsx
+++ src/app/pages/TwentyThreePage.tsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components"
 
 export const TwentyThreePage = () => {
+
+    const [screenHeight, setScreenHeight] = useState<number>(window.innerHeight);
+
+    const setPhoneScreenHeight = (): void => {
+      setScreenHeight(window.innerHeight);
+    };
+  
+    useEffect(() => {
+      const handleResize = () => {
+        requestAnimationFrame(() => {
+          setPhoneScreenHeight();
+        });
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, [])
+    
     return (
-        <Container>
+        <Container screenHeight={screenHeight}>
             <TopRow>
-            <AlignmentPattern>
-                <AlignmentPatternInside/>
-            </AlignmentPattern>
-            <AlignmentPattern>
-                <AlignmentPatternInside/>
-            </AlignmentPattern>
+                <AlignmentPattern>
+                    <AlignmentPatternInside/>
+                </AlignmentPattern>
+                <AlignmentPattern>
+                    <AlignmentPatternInside/>
+                </AlignmentPattern>
             </TopRow>
             <AlignmentPattern>
                 <AlignmentPatternInside/>
@@ -18,14 +40,20 @@ export const TwentyThreePage = () => {
     )
 }
 
-const Container = styled.div`
+interface ContainerProps {
+    screenHeight: number;
+}
+
+const Container = styled.div<ContainerProps>`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    height: 100vh;
+    overflow-y: hidden;
 
     padding: 0;
     margin: 0;
+
+    height: ${(props) => props.screenHeight}px;
 `
 
 const AlignmentPattern = styled.div`
