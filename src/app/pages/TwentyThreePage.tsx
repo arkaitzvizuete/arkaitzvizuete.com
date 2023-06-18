37997a54c5e5d4394ba5f4fa95cdae8f7b6e3c89
Arkaitz Vizuete <code@arkaitzvizuete.com>
2023-06-19 00:34:15 +0200

    Update TwentyThreePage.tsx

--- src/app/pages/TwentyThreePage.tsx
+++ src/app/pages/TwentyThreePage.tsx
@@ -1,7 +1,51 @@
+import styled from "styled-components"
+
 export const TwentyThreePage = () => {
     return (
-        <div>
-             <h1>23</h1>
-        </div>
+        <Container>
+            <TopRow>
+            <AlignmentPattern>
+                <AlignmentPatternInside/>
+            </AlignmentPattern>
+            <AlignmentPattern>
+                <AlignmentPatternInside/>
+            </AlignmentPattern>
+            </TopRow>
+            <AlignmentPattern>
+                <AlignmentPatternInside/>
+            </AlignmentPattern>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100vh;
+    
+    padding: 0;
+    margin: 0;
+`
+
+const AlignmentPattern = styled.div`
+    height: 50px;
+    width: 50px;
+
+    border: 10px solid black;
+`
+
+const AlignmentPatternInside = styled.div`
+    height: 30px;
+    width: 30px;
+    
+    background: black;
+
+    margin: 10px;
+`
+
+const TopRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
