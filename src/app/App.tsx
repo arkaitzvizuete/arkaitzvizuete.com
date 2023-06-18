8ac986c44bb40af2cb01a40407fd7571c16f55f3
Arkaitz Vizuete <code@arkaitzvizuete.com>
2023-06-18 19:13:56 +0200

    Refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,14 @@ import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/23" element={<TwentyThreePage/>} />
+      <Route path='/*' element={<NotFoundPage/>} />
+    </Routes>
+    <App />
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage } from './app/pages/HomePage';
-import { NotFoundPage } from './app/pages/NotFoundPage';
-import { TwentyThreePage } from './app/pages/TwentyThreePage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<HomePage/>} />
-    <Route path="/23" element={<TwentyThreePage/>} />
-    <Route path='/*' element={<NotFoundPage/>} />
-  </Routes>
-  <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
